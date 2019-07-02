6b366c2f6171543d39deb5d8612ee547df2803ea
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-07-03 02:00:16 +0430

    Update ui.c

--- ui.c
+++ ui.c
@@ -23,7 +23,7 @@ int gameisavailable(char gamename[]){
 void ui(){
     printf("\n Console Starting...");
     sleep(1);
-    while(gamename != "quit"){
+    while(1){
         system("cls");
         printf("\n Enter the name of game\n ");
         gets(gamename);
@@ -32,7 +32,7 @@ void ui(){
             getchar();
         }
         else{
-            printf("\n Game successfully loaded.\n ");
+            printf("\n Game successfully imported.\n ");
             sleep(1);
             return;
 
